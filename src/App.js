3be269c9dc94718cd431912b6a314336f48823c5
ovidiu.grigoras <ovidiu.grigoras@digitalya.ro>
2016-06-01 16:42:20 +0300

    contact list finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import {render} from 'react-dom';
+import ContactsList from './ContactsList';
+
+let contacts = [{
+    id: 1,
+    name: 'Ovidiu',
+    phone: '0753 444 555'
+},
+{
+    id: 2,
+    name: 'Alex',
+    phone: '0753 231 555'
+},
+{
+    id: 3,
+    name: 'Jeff',
+    phone: '0753 444 222'
+},
+{
+    id: 4,
+    name: 'Tim',
+    phone: '0722 4334 555'
+}
+]
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Contact List</h1>
+                <ContactsList contacts={this.props.contacts}/>
             </div>
         )
     }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App contacts={contacts} />, document.getElementById('app'));
